2308bf7eba7975d321020691b9247ae369d698dc
Matthias <113436395+Matthiasbrat@users.noreply.github.com>
2024-06-24 08:20:33 +0200

    feat(init): first commit

---
+++ src/test/java/ch/vs/com/java/demo/server/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package ch.vs.com.java.demo.server;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
